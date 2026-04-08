937428970b1ebca459e809bc0fdff7f23ba961b3
jerasto-var <jerasto.vargri@gmail.com>
2026-04-08 01:51:52 +0000

    Deploying to gh-pages from @ jerasto-var/lectoescritura-basica@174e1b3c7b2bcf416f98242c52256cb89942a143 🚀

--- words.js
+++ words.js
@@ -1,4 +1,5 @@
 const leccionesData = {
   "ballena_azul": { "Copiado": 33, "Lectura": 23, "Dictado": 18 },
   "el_kraken": { "Copiado": 46, "Lectura": 45, "Dictado": 47 },
+  "tarantula_goliath": { "Copiado": 0, "Lectura": 0, "Dictado": 0 },
 };
